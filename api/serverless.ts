448910bb68bbc4de5369708e02591524516bd560
Moroine <moroine.bentefrit@gmail.com>
2023-05-23 13:36:28 +0200

    chore: eslint + prettier

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,24 +1,21 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import hello from '@functions/hello';
+import hello from "./src/functions/hello";
 
 const serverlessConfiguration: AWS = {
-  service: 'order-picker',
-  frameworkVersion: '3',
-  plugins: [
-    'serverless-esbuild',
-    "serverless-offline",
-  ],
+  service: "order-picker",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild", "serverless-offline"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
   },
   // import the function via paths
@@ -29,10 +26,10 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
     },
   },
